9f6cc10439718d1ef9412a5f9a06a741abe5bfb8
beaJenius <beajenius@yandex.ru>
2022-01-04 23:21:59 +0300

    docs: task is completed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ module.exports = function reverse (n) {
   if (innumber < 0) {
     innumber = innumber * -1;
   }
-  console.log("number=" + innumber);
-
   let str = String(innumber).split('').reverse().join('');
-
-
-  console.log("str=" + str);
   return Number(str);
 }
